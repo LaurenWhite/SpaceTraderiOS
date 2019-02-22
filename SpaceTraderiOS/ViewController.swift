6e29c29a17b9d44cf87e6b597945034842e9ca53
LaurenWhite <lauren5826@gmail.com>
2019-02-22 18:18:50 -0500

    Initial Commit

---
+++ SpaceTraderiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceTraderiOS
+//
+//  Created by Lauren White on 2/22/19.
+//  Copyright © 2019 Lauren White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
